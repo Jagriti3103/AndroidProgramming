2a23c283f6b3e8bd1fc228c21be407663c0e60bf
Jagriti3103 <jgupta.31103@gmail.com>
2024-04-08 23:29:48 +0530

    chatbot

---
+++ chatbot/app/src/main/java/com/example/chatbot/chatbot.kt
@@ -0,0 +1,13 @@
+package com.example.chatbot
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import com.example.chatbot.components.ChatHeader
+
+@Composable
+fun ChatBot() {
+    Column {
+        ChatHeader()
+    }
+    
+}
