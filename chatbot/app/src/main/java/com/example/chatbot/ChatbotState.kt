dc7137186377a3bb277ed1a56defd19fbd96519f
JagritiAG <138863703+Jagriti3103@users.noreply.github.com>
2024-04-09 16:56:50 +0530

    Delete chatbot directory

--- chatbot/app/src/main/java/com/example/chatbot/ChatbotState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.chatbot
-
-sealed interface ChatbotUiState{
-
-    data object Ideal : ChatbotUiState
-    data object Loading : ChatbotUiState
-    data class Success(val ChatData: String): ChatbotUiState
-    data class Error(val ChatError: String): ChatbotUiState
-}
--- chatbot/app/src/main/java/com/example/chatbot/chatbotVM.kt
+++
@@ -1,4 +0,0 @@
-package com.example.chatbot
-
-class chatbotVM {
-}
