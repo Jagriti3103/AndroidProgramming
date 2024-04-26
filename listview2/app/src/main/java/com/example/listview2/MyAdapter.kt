f0ddddedb56a47256412ad4e3b4cd320732571ff
Jagriti <jgupta.31103@gmail.com>
2024-04-26 21:42:24 +0530

    new

--- listview2/app/src/main/java/com/example/listview2/MyAdapter.kt
+++ listview2/app/src/main/java/com/example/listview2/MyAdapter.kt
@@ -1,13 +1,31 @@
 package com.example.listview2
 
 import android.app.Activity
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import de.hdodenhof.circleimageview.CircleImageView
 
-class MyAdapter(val context :Activity , val arrayList: ArrayList<User>) : ArrayAdapter<User>(context, R.layout.eachitem,arrayList)
+class MyAdapter(val context :Activity , val arrayList: ArrayList<User>) :
+    ArrayAdapter<User>(context, R.layout.eachitem,arrayList)
 {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return super.getView(position, convertView, parent)
+
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.eachitem,null)
+
+        val image = view.findViewById<CircleImageView>(R.id.profile_image)
+        val name = view.findViewById<TextView>(R.id.name)
+        val lastMsg = view.findViewById<TextView>(R.id.chat)
+        val lastmsgtime = view.findViewById<TextView>(R.id.chatTime)
+
+        name.text = arrayList[position].name
+        lastMsg.text = arrayList[position].msg
+        lastmsgtime.text = arrayList[position].msgtime
+
+        image.setImageResource(arrayList[position].imgid)
+        return view
     }
 }
